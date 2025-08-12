151a505d5dafe0c0f97383014cc40d662ca77469
User <ssaminxojayev176@gmail.com>
2025-08-12 09:33:10 +0500

    news

---
+++ main.py
@@ -0,0 +1,13 @@
+def parol_tekshir(parol):
+    if len(parol) < 8:
+        return "Parol juda qisqa!"
+    elif parol.isdigit():
+        return "Parol faqat raqamlardan iborat bolishi mumkin emas!"
+    elif parol.isalpha():
+        return "Parol faqat harflardan iborat bolishi mumkin emas!"
+    else:
+        return "Parol qabul qilindi."
+
+print(parol_tekshir("123456"))
+print(parol_tekshir("abcdefg"))
+print(parol_tekshir("abc12345"))
